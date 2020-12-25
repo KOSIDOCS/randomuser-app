0d2c0f4797ea01a833cc403016e26f3dd435b163
kosidocs <kelvinonaru@gmail.com>
2020-12-25 15:25:45 +0400

    Updates

---
+++ precache-manifest.cf52e824c107896a7fb8b236790323b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4832fffb15dbe8cfd14286cca8d1e603",
+    "url": "/randomuser-app/index.html"
+  },
+  {
+    "revision": "b3cd720881f7adc5b337",
+    "url": "/randomuser-app/static/js/2.e20eab18.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/randomuser-app/static/js/2.e20eab18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ef0a6ba648946c8a8c",
+    "url": "/randomuser-app/static/js/main.6cf67249.chunk.js"
+  },
+  {
+    "revision": "903294909d2e196ffa70",
+    "url": "/randomuser-app/static/js/runtime-main.f6c9b5c5.js"
+  },
+  {
+    "revision": "6cadafc6327ceafcb9efafe2fd020f0a",
+    "url": "/randomuser-app/static/media/undraw_female_avatar_w3jk.6cadafc6.svg"
+  },
+  {
+    "revision": "04f6abe9a89eeb026b925cc4bfeebb64",
+    "url": "/randomuser-app/static/media/undraw_male_avatar_323b.04f6abe9.svg"
+  }
+]);
